107ed50354ef0444c5413ca9eb36a5c80a2451f2
Wuake <xavier.crennpro@gmail.com>
2023-04-13 10:21:50 +0200

    Fin du tp2

--- JPA/src/main/java/fr/sdv/JPA/TestJPA.java
+++ JPA/src/main/java/fr/sdv/JPA/TestJPA.java
@@ -2,10 +2,8 @@ package fr.sdv.JPA;
 
 import fr.sdv.entities.Livre;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import javax.persistence.*;
+import java.util.List;
 
 public class TestJPA {
     public static void main(String[] args) {
@@ -13,13 +11,44 @@ public class TestJPA {
         EntityManager em = emf.createEntityManager();
         EntityTransaction transaction = em.getTransaction();
 
+        //insertion
         Livre livre1 = new Livre();
         livre1.setAuteur("Franck Dubosc");
         livre1.setTitre("Le Slip");
         em.persist(livre1);
 
+        //recherche
         Livre livre2 = em.find(Livre.class, 5);
         System.out.println(livre2);
+
+        //modification
+        //je sais que j'aurais pu prendre le livre 2, c'est volontaire
+        Livre livre3 = em.find(Livre.class, 5);
+        livre3.setTitre("Du plaisir dans la cuisine");
+        em.persist(livre3);
+
+        //JPQL-1
+        Query query = em.createQuery("SELECT l FROM Livre l WHERE l.auteur = :auteur", Livre.class);
+        query.setParameter("auteur","Jules Verne");
+        List<Livre> livres = query.getResultList();
+        System.out.println(livres);
+
+        //JPQL-2
+        Query query2 = em.createQuery("SELECT l FROM Livre l WHERE l.titre = :titre", Livre.class);
+        query2.setParameter("titre","Germinal");
+        List<Livre> livres2 = query2.getResultList();
+        System.out.println(livres2);
+
+        //suppression
+        Livre livre_suppr = em.find(Livre.class, 7);
+        if (livre_suppr != null){
+            em.remove(livre_suppr);
+        }
+
+        //tous les bouquins
+        Query query_livres = em.createQuery("SELECT l FROM Livre l", Livre.class);
+        List<Livre> livres3 = query_livres.getResultList();
+        System.out.println(livres3);
 
         transaction.begin();
         transaction.commit();
